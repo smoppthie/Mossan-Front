a650dac632023e5171acc7856dbd7938dae78820
Joselyn <yoselin.cornejo@alumnos.uv.cl>
2024-10-28 22:50:28 -0300

    Base Front

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Header from './componentes/Header'; 
+
+function App() {
+  return (
+    <div className="App">
+      <header />  {/* Usamos el componente */}
+      <p>Esto es un ejemplo básico de una página en React.</p>
+    </div>
+  );
+}
+
+
+export default App;
