e2e6558cdc23380fb9e843e9dcf75de41495c19e
smoppthie <yoselin.cornejo@alumnos.uv.cl>
2024-12-12 14:55:09 -0300

    boton flotante

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Sidebar from './components/Sidebar';
 import TopBar from './components/TopBar';
 import RouterConfig from './router';
 import { CartProvider } from './components/CartContext';
+import FloatingCartButton from './components/FloatingCartButton'; // Importar el botón flotante
 import './App.css';
 
 function App() {
@@ -13,17 +14,19 @@ function App() {
 
   return (
     <CartProvider>
-    <Router>
-      <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
-        <TopBar />
-        <Box sx={{ display: 'flex', flex: 1 }}>
-          <Sidebar searchText={searchText} setSearchText={setSearchText} /> {/* Pasar el estado de búsqueda */}
-          <Box sx={{ flex: 1 }}>
-            <RouterConfig searchText={searchText} /> {/* Usar RouterConfig para manejar las rutas */}
+      <Router>
+        <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+          <TopBar />
+          <Box sx={{ display: 'flex', flex: 1 }}>
+            <Sidebar searchText={searchText} setSearchText={setSearchText} /> {/* Pasar el estado de búsqueda */}
+            <Box sx={{ flex: 1 }}>
+              <RouterConfig searchText={searchText} /> {/* Usar RouterConfig para manejar las rutas */}
+            </Box>
           </Box>
+          {/* Agregar el botón flotante */}
+          <FloatingCartButton />
         </Box>
-      </Box>
-    </Router>
+      </Router>
     </CartProvider>
   );
 }
